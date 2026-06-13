b181c8118bafd76b0c773b1bece6b38dec9f0b0c
Martin Halden <276353@stud.inn.no>
2026-06-13 19:23:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Roguelite
+
+Developed with Unreal Engine 5
---
+++ Source/Roguelite/Roguelite.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Roguelite.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Roguelite, "Roguelite" );
